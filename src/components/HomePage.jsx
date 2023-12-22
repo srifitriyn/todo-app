0dc5f6172cde5e716437278e202fa9da7c1c9d4a
Sri Fitri Yani Nurhasanah <044592357@ecampus.ut.ac.id>
2023-12-23 00:16:56 +0700

    copy homepage image to public folder due undetected image at src

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ export default function HomePage() {
   return (
     <div className="relative w-full min-h-screen bg-top bg-no-repeat bg-contain bg-bg-light dark:bg-bg-dark">
         <img
-        src="src/assets/hand-drawn-flat-winter-landscape-illustration.jpg"
+        src="/hand-drawn-flat-winter-landscape-illustration.jpg"
         alt="image light"
         className="absolute z-0 object-cover opacity-60 w-full sm:block"
         style={{ height: "250px" }}
